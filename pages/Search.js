de69336e4187919e59f693f39f674f1f83f956e2
ImRayyaan <rnadeem@uci.edu>
2023-05-27 11:37:50 -0700

    Mukarram

--- pages/Search.js
+++ pages/Search.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import Image123 from  '../assets/splash.png';
+import { View, Image, StyleSheet } from 'react-native';
 
 const DetailsPage = () => {
   return (
-    <View>
-      <Text>This is the details page</Text>
+    <View style={styles.container}>
+      <Image
+        source= {Image123}
+        style={styles.image}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  rectangle: {
+    width: 200,
+    height: 100,
+    backgroundColor: 'blue',
+    borderRadius: 10, // Adjust this value to change the roundness of the edges
+  },
+});
 
 export default DetailsPage;
