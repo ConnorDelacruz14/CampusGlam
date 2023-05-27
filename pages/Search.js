3a62de94f14feae77a263678580b229ebf577786
ImRayyaan <rnadeem@uci.edu>
2023-05-27 14:13:16 -0700

    message

--- pages/Search.js
+++ pages/Search.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import Image123 from  '../assets/splash.png';
-import { View, Image, StyleSheet } from 'react-native';
-=======
 import React, { useEffect, useRef } from "react";
 import {
   TextInput,
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
@@ -17,75 +11,61 @@ import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { PanGestureHandler, State } from "react-native-gesture-handler";
 import NavBar from "../components/NavBar";
-function SearchBar() {
+
+class UserRectangles extends React.Component {
+  render() {
+    return <View style={styles.rectangle}></View>;
+  }
+}
+
+export default function Home() {
   const navigation = useNavigation();
   const searchInputRef = useRef(null);
 
   const handlePress = () => {
     navigation.navigate("Home");
   };
-  
 
   useEffect(() => {
     if (navigation.isFocused()) {
       searchInputRef.current.focus();
     }
   }, [navigation]);
->>>>>>> main
 
-  return (
-<<<<<<< HEAD
-    <View style={styles.container}>
-      <Image
-        source= {Image123}
-        style={styles.image}
-      />
-=======
-
-    <View style={styles.searchContainer}>
-      <Feather name="arrow-left" size={24} color="black" style={styles.icon} onPress={handlePress}/>
-      <TextInput
-        ref={searchInputRef}
-        style={styles.input}
-        placeholder="Search CampusGlam"
-      />
-        
->>>>>>> main
-    </View>
-    
-    
-  );
-}
-
-function UserRectangles(){
-
-  return <View style={styles.rectangle}></View>;
-
-}
-
-export default function Home() {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
-        <SearchBar />
+        <View style={styles.searchContainer}>
+          <Feather
+            name="arrow-left"
+            size={24}
+            color="black"
+            style={styles.icon}
+            onPress={handlePress}
+          />
+          <TextInput
+            ref={searchInputRef}
+            style={styles.input}
+            placeholder="Search CampusGlam"
+          />
+        </View>
         <Text style={styles.title}></Text>
-        <UserRectangles></UserRectangles>
+        <View style={styles.rectanglesContainer}>
+          <UserRectangles />
+          <UserRectangles />
+          <UserRectangles />
+        </View>
+        <NavBar />
       </View>
     </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
-
-<<<<<<< HEAD
-const styles = StyleSheet.create({
-  rectangle: {
-    width: 200,
-    height: 100,
-=======
   container: {
     flex: 1,
-    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
   },
   searchContainer: {
     flexDirection: "row",
@@ -96,7 +76,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     margin: 10,
-    position: 'absolute',
+    position: "absolute",
     top: 80,
     left: 30,
     width: 350,
@@ -112,17 +92,17 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "bold",
+    marginVertical: 10,
+  },
+  rectanglesContainer: {
+    marginTop: 20,
+    alignItems: "center",
   },
   rectangle: {
     width: 350,
     height: 160,
->>>>>>> main
-    backgroundColor: 'blue',
-    borderRadius: 10, // Adjust this value to change the roundness of the edges
+    backgroundColor: "white",
+    borderRadius: 10,
+    marginBottom: 65, // Adjust this value to change the spacing between the rectangles
   },
 });
-<<<<<<< HEAD
-
-export default DetailsPage;
-=======
->>>>>>> main
