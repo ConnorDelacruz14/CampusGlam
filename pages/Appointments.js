03d1483937be4f4301f3f70d2271771bf4622d69
Matthew Phan <phannm@uci.edu>
2023-05-27 12:36:06 -0700

    navbar

---
+++ pages/Appointments.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+
+const Page = () => {
+  return (
+    <View style={styles.container}>
+      <Text>This page is for Appointments</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+});
+
+export default Page;
