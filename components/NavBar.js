f3cc9c26970801d477e041610d7142425dc103f3
Connor Delacruz <connord1432@gmail.com>
2023-05-27 21:23:56 -0700

    navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+import { Text, View, TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
